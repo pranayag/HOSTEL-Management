8199c58491b414cb6daed024f78a8513570505dc
dipti <dptksh@gmail.com>
2020-10-14 00:33:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
   <div style= "text-align: center; color: white;font-size: 40px;font-family:'Orbitron', sans-serif;
   "><h2>Q.me</h2>
     <h1>YE TO MERA PROJECT HAI</h1>
-    
+    <h1>Q.me is question bank portal for students preparing for competitive exams</h1>
 
   </div>
 </div>
